a8892aa0e1a2beb06f80a80a57dd7c4777015f91
tsuyoii <49632080+tsuyoii@users.noreply.github.com>
2020-01-07 23:53:00 +0800

    Update PalindromeFinder.java

--- prj1b/PalindromeFinder.java
+++ prj1b/PalindromeFinder.java
@@ -1,12 +1,13 @@
 import com.example.one.prj0.In;
+
 //找出文件中字符数大于等于4并且是回文的单词并显示
 public class PalindromeFinder {
     public static void main(String []args) {
         int minlength = 4;
+        In a = new In("./words.txt");
         Palindrome palindrome = new Palindrome();
-        In in = new In();
-        while (in.isEmpty()) {
-            String word = in.readString();
+        while (!a.isEmpty()) {
+            String word = a.readString();
             if (word.length() >= 4 && palindrome.isPalindrome(word)) {
                 System.out.println(word);
             }
