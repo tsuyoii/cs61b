b8579f9d718aa499efab1a69900ec8f647d1c627
tsuyoii <49632080+tsuyoii@users.noreply.github.com>
2020-01-07 22:55:15 +0800

    Create testOffByN.java

---
+++ prj1b/testOffByN.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class testOffByN {
+    @Test
+    public void testOffByN(){
+        OffByN oN = new OffByN(5);
+        assertTrue(oN.equalChars('f', 'a'));
+        assertFalse(oN.equalChars('z', 'x'));
+    }
+}
