ab38d9bdf81347428bf2c9f9392eaff5c8c35084
tsuyoii <49632080+tsuyoii@users.noreply.github.com>
2020-01-07 22:50:40 +0800

    Create OffByN.java

---
+++ prj1b/OffByN.java
@@ -0,0 +1,11 @@
+public class OffByN implements CharacterComparator{
+    //由用户自己输入参数N
+    private int offset;
+    public OffByN(int N){
+        this.offset = N;
+    }
+    @Override
+     public boolean equalChars(char x, char y) {
+        return Math.abs(x-y)==offset;
+    }
+}
